e6b6268705c65781b233335277630aca517c7f96
StarKeyJON <83514596+StarKeyJON@users.noreply.github.com>
2021-06-07 08:51:04 -0700

    Add files via upload

---
+++ source.js
@@ -0,0 +1,8 @@
+const sources={'UNI1':'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+'MDEX':'https://api.thegraph.com/subgraphs/name/wetitpig-cross-chain/mdex-bsc',
+'PANCAKE':'https://api.thegraph.com/subgraphs/name/depayfi/pancake-v2-exchange',
+'SUSHI':'https://api.thegraph.com/subgraphs/name/sushiswap/exchange',
+'QUICK':'https://api.thegraph.com/subgraphs/name/sameepsi/quickswap',
+'HONEY':'https://api.thegraph.com/subgraphs/name/1hive/honeyswap-v2'};
+
+module.exports = { sources };
