00350dab57ba1a8abeec7ea952a9764668b7f5c2
StarKeyJON <83514596+StarKeyJON@users.noreply.github.com>
2021-06-07 09:15:52 -0700

    Update and rename script.js to modularScript.js
    updated with comments

--- modularScript.js
+++ modularScript.js
@@ -1,18 +1,23 @@
 const axios = require('axios')
 const ObjectsToCsv = require('objects-to-csv');
 const { sources } = require("./source.js");
-  
+
+//Create new variables from the key:values object in sources
 const keys = Object.keys(sources);
 const values = Object.values(sources);
 
+//iterate over each object in keys and initiate function
 keys.forEach(function (keys) {
+//for each key item, iterate over each object in values and initiate the main function
 values.forEach(function(values) {
+  //send the fetch request and save the returned results to a new csv file
   const MAIN = async () => {
   try {
-    
   const result = await axios.post(
-      values, 
+      values, //api link variable
       {
+        //I will create this as a variable that the user chooses eventually
+        //some graph schemas are structured differently as well
           query: `
           {
               swaps {
@@ -38,10 +43,8 @@ values.forEach(function(values) {
       } 
     )
     const csv = new ObjectsToCsv(result.data.data.swaps)
-
-    await csv.toDisk(`/datasets/${keys}.csv`)
-
-       
+  //saving each returned object into a new csv file named with its own variable
+    await csv.toDisk(`/datasets/${keys}.csv`) 
     } catch(error) {
       console.log(error);
     }
