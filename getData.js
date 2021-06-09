c4ac519171d70773d4bc14a3d6a36aaccc2fa71a
StarKeyJON <83514596+StarKeyJON@users.noreply.github.com>
2021-06-09 11:41:48 -0700

    Update getData.js
    Created a schema variable and completed the forEach function.
    I need to create a function that grabs the timestamp from the first row of each csv file collected and held in state to check against the next csv file that comes in, and if it is the same then exclude that key from the search argument.

--- getData.js
+++ getData.js
@@ -2,60 +2,39 @@ const axios = require('axios')
 const ObjectsToCsv = require('objects-to-csv');
 const { sources } = require("./source.js");
 const fs = require('fs');
-  
-const keys = Object.keys(sources);
-const values = Object.values(sources);
+const { defaultSchema } = require("./queries.js");
 
-keys.forEach(function (keys) {
-  values.forEach(function(values) {
-    const MAIN = async () => {
-    try {
-    const result = await axios.post(
-        values, 
-        {
-            query: `
-            {
-                swaps(orderBy: timestamp, orderDirection: desc){
-                  timestamp
-                  pair {
-                    token0 {
-                        name
-                    }
-                    token1 {
-                        name
-                    }
-                  }
-                  sender
-                  amount0In
-                  amount1In
-                  amount0Out
-                  amount1Out
-                  to
-                  amountUSD
-                }
-              }
-            `
-        } 
-      )
+function POST() {
+  Object.entries(sources).forEach(([key, value]) => {
+    MAIN = async () => {
+      try {
+      const result = await axios.post(
+          value, 
+          {
+              query: `
+              ${defaultSchema}
+              `
+          } 
+        )
         const csv = new ObjectsToCsv(result.data.data.swaps)
-      
-        await csv.toDisk(`./${keys}.csv`)
-
-      } catch(error) {
-        //Error logging
-        fs.stat('errorLog.txt', function (err, stat) {
-          if (err == null) {
-            fs.appendFile('errorLog.txt', csv, function (err) {
-              if (err) throw err;
-            });
-          } else if (err.code == 'ENOENT') {
-            fs.writeFile('errorLog.csv', err, function (err) {
-              if (err) throw err;
-            });
-          }
-        });
-      }
-    };
+        
+        await csv.toDisk(`./${key}.csv`)
+        
+        } catch(err) {
+          //Error logging
+          fs.stat('errorLog.txt', function (err, stat) {
+            if (err == null) {
+              fs.appendFile('errorLog.txt', err&&stat, function (err) {
+                if (err) throw err;
+              });
+            } else if (err.code == 'ENOENT') {
+              fs.writeFile('errorLog.csv', err&&stat, function (err) {
+                if (err) throw err;
+              });
+            }
+          });
+        }
+      };
     MAIN();
-  })
-});
+  })}
+POST();
