c39846dc8b5fef43e36df3c4f56fb4c71be6cf12
StarKeyJON <83514596+StarKeyJON@users.noreply.github.com>
2021-06-09 11:44:55 -0700

    Add files via upload

---
+++ queries.js
@@ -0,0 +1,26 @@
+const defaultSchema = (
+`
+{
+    swaps(orderBy: timestamp, orderDirection: desc){
+      timestamp
+      pair {
+        token0 {
+            name
+        }
+        token1 {
+            name
+        }
+      }
+      sender
+      amount0In
+      amount1In
+      amount0Out
+      amount1Out
+      to
+      amountUSD
+    }
+  }
+`);
+
+
+module.exports = { defaultSchema };
