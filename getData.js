5b050ac896e46d7830707198bb886b5d11a568a1
StarKeyJON <83514596+StarKeyJON@users.noreply.github.com>
2021-06-08 23:22:51 -0700

    Update getData.js

--- getData.js
+++ getData.js
@@ -1,70 +1,61 @@
 const axios = require('axios')
 const ObjectsToCsv = require('objects-to-csv');
 const { sources } = require("./source.js");
-
-//Create new variables from the key:values object in sources
+const fs = require('fs');
+  
 const keys = Object.keys(sources);
 const values = Object.values(sources);
 
-//Create sleep function
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-const postData = keys.forEach(function (keys) {
-  do {
-  //iterate over each object in keys and initiate function
-  keys.forEach(function (keys) {
-  //for each key item, iterate over each object in values and initiate the main function
+keys.forEach(function (keys) {
   values.forEach(function(values) {
-   //send the fetch request and save the returned results to a new csv file
-   const MAIN = async () => {
-   try {
-   const result = await axios.post(
-       values, //api link variable 
+    const MAIN = async () => {
+    try {
+    const result = await axios.post(
+        values, 
         {
-          //I will create this as a variable that the user chooses eventually
-          //some graph schemas are structured differently as well
-           query: `
-           {
-               swaps(orderBy: timestamp, orderDirection: desc){
-                 timestamp
-                 pair {
-                   token0 {
-                       name
-                   }
-                   token1 {
-                       name
-                   }
-                 }
-                 sender
-                 amount0In
-                 amount1In
-                 amount0Out
-                 amount1Out
-                 to
-                 amountUSD
-               }
-             }
-           `
-       } 
+            query: `
+            {
+                swaps(orderBy: timestamp, orderDirection: desc){
+                  timestamp
+                  pair {
+                    token0 {
+                        name
+                    }
+                    token1 {
+                        name
+                    }
+                  }
+                  sender
+                  amount0In
+                  amount1In
+                  amount0Out
+                  amount1Out
+                  to
+                  amountUSD
+                }
+              }
+            `
+        } 
       )
-     const csv = new ObjectsToCsv(result.data.data.swaps)
-    //saving each returned object into a new csv file named with its own variable
-     await csv.toDisk(`/datasets/${keys}.csv`)
-     
-     sleep(15000);
-     
+        const csv = new ObjectsToCsv(result.data.data.swaps)
+      
+        await csv.toDisk(`./${keys}.csv`)
+
       } catch(error) {
-        pass;
+        //Error logging
+        fs.stat('errorLog.txt', function (err, stat) {
+          if (err == null) {
+            fs.appendFile('errorLog.txt', csv, function (err) {
+              if (err) throw err;
+            });
+          } else if (err.code == 'ENOENT') {
+            fs.writeFile('errorLog.csv', err, function (err) {
+              if (err) throw err;
+            });
+          }
+        });
       }
     };
     MAIN();
   })
-} while (true);
 });
-postData();
